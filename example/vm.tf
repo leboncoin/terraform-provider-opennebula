4ee586bf76139ce5915e4cb0f8505ccab8b86c60
Kribl, Dennis <d.kribl@immowelt.de>
2018-05-29 11:43:04 +0200

    extend example/vm.tf with ip segment, refine formatting

--- example/vm.tf
+++ example/vm.tf
@@ -1,26 +1,18 @@
 resource "opennebula_vm" "vm-base" {
-  # all properties listed here are mandatory for creating a vm with a basic template
-
-  # basic informations:
-  count       = "1"    # count of instances to create
-  name        = "MyVM" # custom name of the vm to create
-  template_id = "1"    # template_id to use. Can also be output id of resource_template 
-  permissions = "640"
-
-  # vm properties section:
-  cpu    = "1"    # cpu count 
-  vcpu   = "1"    # vcpu count
-  memory = "1024" # memory count in mb
-
-  # disk section: 
-  image        = "Debian 9.3" # example image name stored in opennebula
-  size         = "20480"      # image size in mb
-  image_driver = "qcow2"      # image driver of the image to use
-  image_uname  = "oneadmin"   # owner of the image to use
-
-  #network section: 
-  network               = "MyCustomNetwork" # network name stored in opennebula
-  network_search_domain = "mycorp.com"      # search Domain 
-  network_uname         = "oneadmin"        # owner of the network to use
-  security_group_id     = "0"               # security group id to use
+  count                 = "1"               # required | count of instances to create
+  name                  = "MyVM"            # required | custom name of the vm to create
+  template_id           = "1"               # required | template_id to use. Can also be output id of resource_template 
+  permissions           = "640"             # required | permissions for the vm (unix-style)
+  cpu                   = "1"               # required | cpu count 
+  vcpu                  = "1"               # required | vcpu count
+  memory                = "1024"            # required | memory count in mb
+  image                 = "Debian 9.3"      # required | example image name stored in opennebula
+  size                  = "20480"           # required | image size in mb
+  image_driver          = "qcow2"           # required | image driver of the image to use
+  image_uname           = "oneadmin"        # required | owner of the image to use
+  network               = "MyCustomNetwork" # required | network name stored in opennebula
+  network_search_domain = "mycorp.com"      # required | search Domain 
+  network_uname         = "oneadmin"        # required | owner of the network to use
+  security_group_id     = "0"               # required | security group id to use
+  ip                    = "0.0.0.0"         # optional | ip to use in network segment
 }
